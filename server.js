182136bcd7b020a9cae73e7794279f6af4398ea1
nlokantha <nlokantha@gmail.com>
2025-10-15 10:29:54 +0530

    3 try

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors())
 app.use(express.json())
 
 app.get("/", (req, res) => {
-  res.send("Hello World!!!! sec try to deploy")
+  res.send("Hello World!!!! 3 try to deploy")
 })
 
 app.listen(PORT, () => {
